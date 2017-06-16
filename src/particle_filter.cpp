b14df8a0e46c6b70ab212820ad76c8075d5eb5cf
Ian Zhang <ianzhang1986@gmail.com>
2017-06-16 10:07:31 -0700

    [Feature] Increase particle number to 100 with better error performance

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -24,7 +24,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1.
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
-	num_particles = 10;
+	num_particles = 100;
 
 	// Taking into account Gaussian Sensor Noise around initial heading estimation
 	double std_x = std[0];
